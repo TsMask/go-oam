7dd6516d27d3df1a58b03c21809249cdfbc23665
TsMask <340112800@qq.com>
2025-07-15 16:58:46 +0800

    ref: 日期工具

--- src/framework/utils/date/date.go
+++ src/framework/utils/date/date.go
@@ -4,32 +4,13 @@ import (
 	"time"
 )
 
-const (
-	// 年 列如：2022
-	YYYY = "2006"
-	// 年-月 列如：2022-12
-	YYYY_MM = "2006-01"
-	// 年-月-日 列如：2022-12-30
-	YYYY_MM_DD = "2006-01-02"
-	// 年月日时分秒 列如：20221230010159
-	YYYYMMDDHHMMSS = "20060102150405"
-	// 年-月-日 时:分:秒 列如：2022-12-30 01:01:59
-	YYYY_MM_DD_HH_MM_SS = "2006-01-02 15:04:05"
-	// 年-月-日T时:分:秒Z时区 列如：2022-12-30T01:01:59+08:00
-	YYYY_MM_DDTHH_MM_SSZ = time.RFC3339
-)
-
-// 格式时间字符串
-//
-// dateStr 时间字符串
-//
-// formatStr 时间格式 默认YYYY-MM-DD HH:mm:ss
+// ParseStrToDate 根据时间字符串解析时间 时间格式看time.DateTime
 func ParseStrToDate(dateStr, formatStr string) time.Time {
 	if dateStr == "" || dateStr == "<nil>" {
 		return time.Time{}
 	}
 	if formatStr == "" {
-		formatStr = YYYY_MM_DD_HH_MM_SS
+		formatStr = time.DateTime
 	}
 	t, err := time.Parse(formatStr, dateStr)
 	if err != nil {
@@ -38,40 +19,25 @@ func ParseStrToDate(dateStr, formatStr string) time.Time {
 	return t
 }
 
-// 格式时间
-//
-// date 可转的Date对象
-//
-// formatStr 时间格式 默认YYYY-MM-DD HH:mm:ss
-func ParseDateToStr(date any, formatStr string) string {
-	t, ok := date.(time.Time)
-	if !ok {
-		switch v := date.(type) {
-		case int64:
-			if v == 0 {
-				return ""
-			}
-			if v > 1e13 {
-				t = time.UnixMilli(v)
-			} else if v > 1e10 {
-				t = time.Unix(v, 0)
-			} else {
-				return ""
-			}
-		case string:
-			parsedTime, err := time.Parse(formatStr, v)
-			if err != nil {
-				return ""
-			}
-			t = parsedTime
-		default:
-			return ""
-		}
+// ParseNumberToDate 根据时间数字时间戳解析时间
+func ParseNumberToDate(dateV int64) time.Time {
+	t := time.Time{}
+	if dateV == 0 {
+		return t
 	}
-	return t.Format(formatStr)
+	if dateV > 1e15 {
+		t = time.UnixMicro(dateV)
+	} else if dateV > 1e12 {
+		t = time.UnixMilli(dateV)
+	} else if dateV > 1e9 {
+		t = time.Unix(dateV, 0)
+	} else {
+		return t
+	}
+	return t
 }
 
-// 格式时间成日期路径
+// ParseDatePath 格式时间成日期路径
 //
 // 年/月 列如：2022/12
 func ParseDatePath(date time.Time) string {
